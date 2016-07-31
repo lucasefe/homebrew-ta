fcd21754eac7d66a930f35ab161d9dafe22b801c
Lucas Florio <lucasefe@users.noreply.github.com>
2016-07-31 12:05:41 -0700

    Update ta.rb

--- ta.rb
+++ ta.rb
@@ -3,7 +3,7 @@ require "formula"
 class Ta < Formula
   homepage "https://github.com/lucasefe/ta#readme"
   url "https://github.com/lucasefe/ta/releases/download/v0.3.0/ta-darwin-amd64.tar.gz"
-  sha256 "87b20f462625302c80b5d8aeca3437c96a97828d3d99a99906d6e9be81b5f651"
+  sha256 "e902b27a191b1f6801783f0b28354691526448283b35b4ce1cad5139fbb2fa05"
 
   def install
     bin.install "ta" => "ta"
